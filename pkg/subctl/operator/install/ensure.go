4d68a37b08fde9aaf84df64c45e316874f7d03c1
Miguel Angel Ajo <majopela@redhat.com>
2019-11-15 16:34:45 +0100

    Handle OpenShift's privileged SCC
    When installing on top of OpenShift the operator service
    account must be added to the privileged scc, otherwise it
    won't be able to create privileged pods.

--- pkg/subctl/operator/install/ensure.go
+++ pkg/subctl/operator/install/ensure.go
@@ -3,11 +3,13 @@ package install
 import (
 	"fmt"
 
+	"k8s.io/client-go/rest"
+
 	"github.com/submariner-io/submariner-operator/pkg/subctl/operator/install/crds"
 	"github.com/submariner-io/submariner-operator/pkg/subctl/operator/install/deployment"
 	"github.com/submariner-io/submariner-operator/pkg/subctl/operator/install/namespace"
+	"github.com/submariner-io/submariner-operator/pkg/subctl/operator/install/scc"
 	"github.com/submariner-io/submariner-operator/pkg/subctl/operator/install/serviceaccount"
-	"k8s.io/client-go/rest"
 )
 
 func Ensure(config *rest.Config, operatorNamespace string, operatorImage string) error {
@@ -28,6 +30,12 @@ func Ensure(config *rest.Config, operatorNamespace string, operatorImage string)
 		return err
 	} else if created {
 		fmt.Printf("* Created operator service account and role\n")
+	}
+
+	if created, err := scc.Ensure(config, operatorNamespace); err != nil {
+		return err
+	} else if created {
+		fmt.Printf("* Updated the privileged SCC\n")
 	}
 
 	if created, err := deployment.Ensure(config, operatorNamespace, operatorImage); err != nil {
