f929e4d4f040425ff6195aecd6012b17b82e8865
Miguel Angel Ajo <majopela@redhat.com>
2019-11-28 13:44:40 +0100

    Fixes subctl compatibility issue with GKE kubeconfig files
    This enables compilation of the required auth plugins used
    in google cloud.
    
    Fixes Issue: #67

--- pkg/subctl/cmd/root.go
+++ pkg/subctl/cmd/root.go
@@ -14,6 +14,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 
