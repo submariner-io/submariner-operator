a8d22810274bc8907b455672bf0a9c216709d930
Stephen Kitt <skitt@redhat.com>
2020-01-16 17:28:20 +0100

    Deploy the KubeFed operator
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ pkg/subctl/operator/common/serviceaccount/ensure.go
@@ -0,0 +1,45 @@
+/*
+© 2019 Red Hat, Inc. and others.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package serviceaccount
+
+import (
+	"fmt"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	v1meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+//Ensure creates the given service account
+func Ensure(restConfig *rest.Config, namespace string, serviceAccount string) (bool, error) {
+	clientSet, err := clientset.NewForConfig(restConfig)
+	if err != nil {
+		return false, err
+	}
+
+	sa := &v1.ServiceAccount{ObjectMeta: v1meta.ObjectMeta{Name: serviceAccount}}
+	_, err = clientSet.CoreV1().ServiceAccounts(namespace).Create(sa)
+	if err == nil {
+		return true, nil
+	} else if errors.IsAlreadyExists(err) {
+		return false, nil
+	} else {
+		return false, fmt.Errorf("ServiceAccount creation failed: %s", err)
+	}
+}
