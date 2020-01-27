85eb917afc14eaa2684cbc3ddc2a94d846734e89
Miguel Angel Ajo <majopela@redhat.com>
2020-01-27 13:43:35 +0100

    Normalize the usage of exitOnError
    onExitError was used withouth formatting "%s" in many places in the
    code, and with %s in some places. It actually expected to receive
    a formating string, resulting on very weird errors when no formattings
    was passed.
    
    This has been simplified to not require formating and just a simple
    message that will be explainatory of the error: "message: err"
    
    Fixes: #118

--- pkg/subctl/cmd/root.go
+++ pkg/subctl/cmd/root.go
@@ -91,10 +91,10 @@ func panicOnError(err error) {
 	}
 }
 
-// exitOnError will print your error nicely and exit
-func exitOnError(format string, err error) {
+// exitOnError will print your error nicely and exit in case of error
+func exitOnError(message string, err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, format, err.Error())
+		fmt.Fprintf(os.Stderr, "%s: %s", message, err.Error())
 		fmt.Fprintln(os.Stderr, "")
 		PrintSubctlVersion(os.Stderr)
 		fmt.Fprintln(os.Stderr, "")
