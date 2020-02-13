c2e39f4fd399bacef635c559155f1a77186198f2
Stephen Kitt <skitt@redhat.com>
2020-02-13 16:42:48 +0100

    Support specifying contexts (#152)
    ... as well as the kubeconfig file. This will be necessary for KIND
    upgrades since KIND (and Kubernetes in general) is moving away from
    separate kubeconfig files.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- pkg/subctl/cmd/root.go
+++ pkg/subctl/cmd/root.go
@@ -41,8 +41,9 @@ import (
 )
 
 var (
-	kubeConfig string
-	rootCmd    = &cobra.Command{
+	kubeConfig  string
+	kubeContext string
+	rootCmd     = &cobra.Command{
 		Use:   "subctl",
 		Short: "An installer for Submariner",
 	}
@@ -54,6 +55,7 @@ func Execute() error {
 
 func init() {
 	rootCmd.PersistentFlags().StringVar(&kubeConfig, "kubeconfig", kubeConfigFile(), "absolute path(s) to the kubeconfig file(s)")
+	rootCmd.PersistentFlags().StringVar(&kubeContext, "kubecontext", "", "kubeconfig context to use")
 }
 
 const (
@@ -102,11 +104,7 @@ func exitOnError(message string, err error) {
 	}
 }
 
-func getClients() (dynamic.Interface, kubernetes.Interface, error) {
-	config, err := clientcmd.BuildConfigFromFlags("", kubeConfig)
-	if err != nil {
-		return nil, nil, err
-	}
+func getClients(config *rest.Config) (dynamic.Interface, kubernetes.Interface, error) {
 	dynClient, err := dynamic.NewForConfig(config)
 	if err != nil {
 		return nil, nil, err
@@ -118,12 +116,19 @@ func getClients() (dynamic.Interface, kubernetes.Interface, error) {
 	return dynClient, clientSet, nil
 }
 
-func getRestConfig() (*rest.Config, error) {
-	return clientcmd.BuildConfigFromFlags("", kubeConfig)
+func getRestConfig(kubeConfigPath string, kubeContext string) (*rest.Config, error) {
+	rules := clientcmd.NewDefaultClientConfigLoadingRules()
+	rules.ExplicitPath = kubeConfigPath
+	rules.DefaultClientConfig = &clientcmd.DefaultClientConfig
+	overrides := &clientcmd.ConfigOverrides{ClusterDefaults: clientcmd.ClusterDefaults}
+	if kubeContext != "" {
+		overrides.CurrentContext = kubeContext
+	}
+	return clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, overrides).ClientConfig()
 }
 
-func handleNodeLabels() error {
-	_, clientset, err := getClients()
+func handleNodeLabels(config *rest.Config) error {
+	_, clientset, err := getClients(config)
 	exitOnError("Unable to set the Kubernetes cluster connection up", err)
 	// List Submariner-labeled nodes
 	const submarinerGatewayLabel = "submariner.io/gateway"
--- scripts/kind-e2e/e2e.sh
+++ scripts/kind-e2e/e2e.sh
@@ -77,7 +77,7 @@ function setup_broker() {
         echo Installing broker on cluster1.
          sd=
          [[ $lighthouse = true ]] && sd=--service-discovery
-         ../bin/subctl --kubeconfig ${PRJ_ROOT}/output/kind-config/dapper/kind-config-cluster1 deploy-broker ${sd} |& cat
+         ../bin/subctl --kubeconfig ${PRJ_ROOT}/output/kind-config/dapper/kind-config-cluster1 --kubecontext cluster1 deploy-broker ${sd} |& cat
          [[ $lighthouse = true ]] && kubefedctl federate namespace default --kubefed-namespace kubefed-operator
     fi
 
@@ -253,6 +253,7 @@ for i in 2 3; do
     if [ "${context}" = "cluster2" ] || [ "${context}" = "cluster3" ]; then
         ../bin/subctl join --operator-image submariner-operator:local \
                         --kubeconfig ${PRJ_ROOT}/output/kind-config/dapper/kind-config-$context \
+                        --kubecontext ${context} \
                         --clusterid ${context} \
                         --repository ${subm_engine_image_repo} \
                         --version ${subm_engine_image_tag} \
@@ -306,6 +307,7 @@ echo "Running subctl a second time to verify if running subctl a second time wor
 
 ../bin/subctl join --operator-image submariner-operator:local \
                 --kubeconfig ${PRJ_ROOT}/output/kind-config/dapper/kind-config-$context \
+                --kubecontext ${context} \
                 --clusterid ${context} \
                 --repository ${subm_engine_image_repo} \
                 --version ${subm_engine_image_tag} \
