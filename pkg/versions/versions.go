35681ef82b75a94f36a52d840eec4a326a2cb285
Miguel Angel Ajo <majopela@redhat.com>
2020-02-14 17:24:48 +0100

    Bump versions to 0.1.0 and centralize versions

---
+++ pkg/versions/versions.go
@@ -0,0 +1,9 @@
+package versions
+
+const (
+	DefaultSubmarinerRepo            = "quay.io/submariner"
+	DefaultSubmarinerOperatorVersion = "0.1.0"
+	DefaultSubmarinerVersion         = "0.1.0"
+	DefaultLighthouseVersion         = "0.1.0"
+	KubeFedVersion                   = "v0.1.0-rc3"
+)
