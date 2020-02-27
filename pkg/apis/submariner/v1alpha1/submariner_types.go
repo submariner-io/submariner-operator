6d9ddbf892cf80d120c26952e2300b1a01791bbc
Alexey Roytman <roytman@il.ibm.com>
2020-02-27 12:04:39 +0200

    Rename Cable -> CableDriver

--- pkg/apis/submariner/v1alpha1/submariner_types.go
+++ pkg/apis/submariner/v1alpha1/submariner_types.go
@@ -45,7 +45,7 @@ type SubmarinerSpec struct {
 	ClusterCIDR              string `json:"clusterCIDR"`
 	GlobalCIDR               string `json:"globalCIDR,omitempty"`
 	Namespace                string `json:"namespace"`
-	CableType                string `json:"cableType,omitempty"`
+	CableDriver              string `json:"cableDriver,omitempty"`
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
