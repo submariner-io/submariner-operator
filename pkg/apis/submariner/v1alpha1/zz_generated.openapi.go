03dca83a831ac820e559c319b125fac923d43129
Alexey Roytman <roytman@il.ibm.com>
2020-02-27 16:03:24 +0200

    small fix

--- pkg/apis/submariner/v1alpha1/zz_generated.openapi.go
+++ pkg/apis/submariner/v1alpha1/zz_generated.openapi.go
@@ -182,6 +182,12 @@ func schema_pkg_apis_submariner_v1alpha1_SubmarinerSpec(ref common.ReferenceCall
 							Format: "",
 						},
 					},
+					"cableDriver": {
+						SchemaProps: spec.SchemaProps{
+							Type:   []string{"string"},
+							Format: "",
+						},
+					},
 				},
 				Required: []string{"ceIPSecDebug", "ceIPSecPSK", "brokerK8sCA", "brokerK8sRemoteNamespace", "brokerK8sApiServerToken", "brokerK8sApiServer", "broker", "natEnabled", "debug", "clusterID", "serviceCIDR", "clusterCIDR", "namespace"},
 			},
