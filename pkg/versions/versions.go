0b89e14d6fe9b1cec1937aa5693346fe0d012c0e
Miguel Angel Ajo <majopela@redhat.com>
2020-03-05 17:19:13 +0100

    Update versions to latest tags
     * submariner to v0.1.1
     * submariner-operator to v0.1.1
     * lighthouse needs no update since there are no code changes between since v0.1.0,
       all changes have been around CI and docs.

--- pkg/versions/versions.go
+++ pkg/versions/versions.go
@@ -2,8 +2,8 @@ package versions
 
 const (
 	DefaultSubmarinerRepo            = "quay.io/submariner"
-	DefaultSubmarinerOperatorVersion = "0.1.0"
-	DefaultSubmarinerVersion         = "0.1.0"
+	DefaultSubmarinerOperatorVersion = "0.1.1"
+	DefaultSubmarinerVersion         = "0.1.1"
 	DefaultLighthouseVersion         = "0.1.0"
 	KubeFedVersion                   = "v0.1.0-rc3"
 )
