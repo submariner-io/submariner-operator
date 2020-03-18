c38e155295cb0b235fcd7860e2a10ee16c7dbb3b
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2020-03-18 17:25:38 +0100

    Update operator versions for 0.2.0-rc0 release

--- pkg/versions/versions.go
+++ pkg/versions/versions.go
@@ -2,8 +2,8 @@ package versions
 
 const (
 	DefaultSubmarinerRepo            = "quay.io/submariner"
-	DefaultSubmarinerOperatorVersion = "0.1.1"
-	DefaultSubmarinerVersion         = "0.1.1"
-	DefaultLighthouseVersion         = "0.1.0"
+	DefaultSubmarinerOperatorVersion = "0.2.0-rc0"
+	DefaultSubmarinerVersion         = "0.2.0-rc0"
+	DefaultLighthouseVersion         = "0.2.0-rc0"
 	KubeFedVersion                   = "v0.1.0-rc3"
 )
