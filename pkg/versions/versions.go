8820cea93b74850eeb50ff334ca30b003e5cb764
Miguel Angel Ajo <majopela@redhat.com>
2020-03-23 16:25:53 +0100

    Bump to release 0.2.0

--- pkg/versions/versions.go
+++ pkg/versions/versions.go
@@ -2,8 +2,8 @@ package versions
 
 const (
 	DefaultSubmarinerRepo            = "quay.io/submariner"
-	DefaultSubmarinerOperatorVersion = "0.2.0-rc1"
-	DefaultSubmarinerVersion         = "0.2.0-rc1"
-	DefaultLighthouseVersion         = "0.2.0-rc0"
+	DefaultSubmarinerOperatorVersion = "0.2.0"
+	DefaultSubmarinerVersion         = "0.2.0"
+	DefaultLighthouseVersion         = "0.2.0"
 	KubeFedVersion                   = "v0.1.0-rc3"
 )
