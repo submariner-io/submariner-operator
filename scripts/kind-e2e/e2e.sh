119d7e488fa65fea35c83c5620342dbeff3a6e88
Mike Kolesnik <mkolesni@redhat.com>
2020-03-31 11:51:54 +0300

    Migrate cluster deployment to shipyard
    Cluster deployment is available in shipyard so use that instead of
    reimplementing it here

--- scripts/kind-e2e/e2e.sh
+++ scripts/kind-e2e/e2e.sh
@@ -1,76 +1,26 @@
 #!/usr/bin/env bash
+
+## Process command line flags ##
+
+source /usr/share/shflags/shflags
+DEFINE_string 'globalnet' 'false' "Deploy with operlapping CIDRs (set to 'true' to enable)"
+DEFINE_string 'lighthouse' 'false' "Deploy with lighthouse"
+DEFINE_string 'status' 'onetime' "Status flag (onetime, create, keep, clean)"
+FLAGS "$@" || exit $?
+eval set -- "${FLAGS_ARGV}"
+
+globalnet="${FLAGS_globalnet}"
+lighthouse="${FLAGS_lighthouse}"
+status="${FLAGS_status}"
+echo "Running with: globalnet=${globalnet}, lighthouse=${lighthouse}, status=${status}"
+
 set -em
 
-source $(git rev-parse --show-toplevel)/scripts/lib/debug_functions
+source ${SCRIPTS_DIR}/lib/debug_functions
+source ${SCRIPTS_DIR}/lib/version
+source ${SCRIPTS_DIR}/lib/utils
 
 ### Functions ###
-
-function kind_clusters() {
-    status=$1
-    lighthouse=$2
-    globalnet=$3
-    version=$4
-    pids=(-1 -1 -1)
-    logs=()
-    for i in 1 2 3; do
-        if [[ $(kind get clusters | grep cluster${i} | wc -l) -gt 0  ]]; then
-            echo Cluster cluster${i} already exists, skipping cluster creation...
-        else
-            logs[$i]=$(mktemp)
-            echo Creating cluster${i}, logging to ${logs[$i]}...
-            (
-            if [[ -n ${version} ]]; then
-                kind create cluster --image=kindest/node:v${version} --name=cluster${i} --config=${PRJ_ROOT}/scripts/kind-e2e/cluster${i}-config.yaml
-            else
-                kind create cluster --name=cluster${i} --config=${PRJ_ROOT}/scripts/kind-e2e/cluster${i}-config.yaml
-            fi
-            master_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cluster${i}-control-plane | head -n 1)
-            sed -i -- "s/user: kubernetes-admin/user: cluster$i/g" $(kind get kubeconfig-path --name="cluster$i")
-            sed -i -- "s/name: kubernetes-admin.*/name: cluster$i/g" $(kind get kubeconfig-path --name="cluster$i")
-            sed -i -- "s/current-context: kubernetes-admin.*/current-context: cluster$i/g" $(kind get kubeconfig-path --name="cluster$i")
-
-            if [[ ${status} = keep ]]; then
-                cp -r $(kind get kubeconfig-path --name="cluster$i") ${PRJ_ROOT}/output/kind-config/local-dev/kind-config-cluster${i}
-            fi
-
-            sed -i -- "s/server: .*/server: https:\/\/$master_ip:6443/g" $(kind get kubeconfig-path --name="cluster$i")
-            cp -r $(kind get kubeconfig-path --name="cluster$i") ${PRJ_ROOT}/output/kind-config/dapper/kind-config-cluster${i}
-            ) > ${logs[$i]} 2>&1 &
-            set pids[$i] = $!
-        fi
-    done
-    if [[ ${#logs[@]} -gt 0 ]]; then
-        echo "(Watch the installation processes with \"tail -f ${logs[*]}\".)"
-        for i in 1 2 3; do
-            if [[ pids[$i] -gt -1 ]]; then
-                wait ${pids[$i]}
-                if [[ $? -ne 0 && $? -ne 127 ]]; then
-                    echo Cluster $i creation failed:
-                    cat ${logs[$i]}
-                fi
-                rm -f ${logs[$i]}
-            fi
-        done
-    fi
-    KUBECONFIG=${PRJ_ROOT}/output/kind-config/dapper/kind-config-cluster1:${PRJ_ROOT}/output/kind-config/dapper/kind-config-cluster2:${PRJ_ROOT}/output/kind-config/dapper/kind-config-cluster3 \
-    kubectl config view --flatten > ${PRJ_ROOT}/output/kind-config/dapper/kind-config-merged
-}
-
-function setup_custom_cni(){
-    declare -A POD_CIDR=( ["cluster2"]="10.245.0.0/16" ["cluster3"]="10.246.0.0/16" )
-    for i in 2 3; do
-        if kubectl --context=cluster${i} wait --for=condition=Ready pods -l name=weave-net -n kube-system --timeout=60s > /dev/null 2>&1; then
-            echo "Weave already deployed cluster${i}."
-        else
-            echo "Applying weave network in to cluster${i}..."
-            kubectl --context=cluster${i} apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')&env.IPALLOC_RANGE=${POD_CIDR[cluster${i}]}"
-            echo "Waiting for weave-net pods to be ready cluster${i}..."
-            kubectl --context=cluster${i} wait --for=condition=Ready pods -l name=weave-net -n kube-system --timeout=700s
-            echo "Waiting for core-dns deployment to be ready cluster${i}..."
-            kubectl --context=cluster${i} -n kube-system rollout status deploy/coredns --timeout=300s
-        fi
-    done
-}
 
 function setup_broker() {
     if kubectl --context=cluster1 get crd clusters.submariner.io > /dev/null 2>&1; then
@@ -82,7 +32,7 @@ function setup_broker() {
          gn=
          [[ $globalnet = true ]] && gn=--globalnet
          set -o pipefail
-         ../bin/subctl --kubeconfig ${PRJ_ROOT}/output/kind-config/dapper/kind-config-merged --kubecontext cluster1 deploy-broker ${sd} ${gn}|& cat
+         ${DAPPER_SOURCE}/bin/subctl --kubeconfig ${PRJ_ROOT}/output/kubeconfigs/kind-config-merged --kubecontext cluster1 deploy-broker ${sd} ${gn}|& cat
          set +o pipefail
          [[ $lighthouse = true ]] && kubefedctl federate namespace default --kubefed-namespace kubefed-operator
     fi
@@ -130,10 +80,11 @@ function create_subm_vars() {
   broker_deployment_name=submariner-k8s-broker
   globalnet_deployment_name=submariner-globalnet
 
-  clusterCIDR_cluster2=10.245.0.0/16
-  clusterCIDR_cluster3=10.246.0.0/16
-  serviceCIDR_cluster2=100.95.0.0/16
-  serviceCIDR_cluster3=100.96.0.0/16
+  declare_cidrs
+  clusterCIDR_cluster2=${cluster_CIDRs['cluster2']}
+  clusterCIDR_cluster3=${cluster_CIDRs['cluster3']}
+  serviceCIDR_cluster2=${service_CIDRs['cluster2']}
+  serviceCIDR_cluster3=${service_CIDRs['cluster3']}
   natEnabled=false
   if [[ $globalnet = true ]]; then
       globalCIDR_cluster2=169.254.0.0/19
@@ -228,62 +179,39 @@ function update_subm_pods() {
 }
 
 function test_with_e2e_tests {
-    cd ../test/e2e
-
-    # Setup the KUBECONFIG env
-    export KUBECONFIG=$(echo ${PRJ_ROOT}/output/kind-config/dapper/kind-config-cluster{1..3} | sed 's/ /:/g')
+    cd ${DAPPER_SOURCE}/test/e2e
 
     go test -args -ginkgo.v -ginkgo.randomizeAllSpecs -ginkgo.reportPassed \
         -dp-context cluster2 -dp-context cluster3  \
-        -report-dir ${DAPPER_SOURCE}/${DAPPER_OUTPUT}/junit 2>&1 | \
-        tee ${DAPPER_SOURCE}/${DAPPER_OUTPUT}/e2e-tests.log
+        -report-dir ${DAPPER_OUTPUT}/junit 2>&1 | \
+        tee ${DAPPER_OUTPUT}/e2e-tests.log
 }
 
 function cleanup {
-    for i in 1 2 3; do
-      if [[ $(kind get clusters | grep cluster${i} | wc -l) -gt 0  ]]; then
-        kind delete cluster --name=cluster${i};
-      fi
-    done
-
-    if [[ $(docker ps -qf status=exited | wc -l) -gt 0 ]]; then
-        echo Cleaning containers...
-        docker ps -qf status=exited | xargs docker rm -f
-    fi
-    if [[ $(docker images -qf dangling=true | wc -l) -gt 0 ]]; then
-        echo Cleaning images...
-        docker images -qf dangling=true | xargs docker rmi -f
-    fi
-#    if [[ $(docker images -q --filter=reference='submariner*:local' | wc -l) -gt 0 ]]; then
-#        docker images -q --filter=reference='submariner*:local' | xargs docker rmi -f
-#    fi
-    if [[ $(docker volume ls -qf dangling=true | wc -l) -gt 0 ]]; then
-        echo Cleaning volumes...
-        docker volume ls -qf dangling=true | xargs docker volume rm -f
-    fi
+    "${SCRIPTS_DIR}"/cleanup.sh
 }
 
 ### Main ###
 
-if [[ $1 = clean ]]; then
+if [[ $status = clean ]]; then
     cleanup
     exit 0
-fi
-
-if [[ $1 != keep ]]; then
+elif [[ $status = onetime ]]; then
+    echo Status $status: Will cleanup on EXIT signal
     trap cleanup EXIT
+elif [[ $status != keep && $status != create ]]; then
+    echo Unknown status: $status
+    cleanup
+    exit 1
 fi
 
-echo Starting with status: $1, lighthouse: $2, globalnet: $3 k8s_version: $4.
 PRJ_ROOT=$(git rev-parse --show-toplevel)
-mkdir -p ${PRJ_ROOT}/output/kind-config/dapper/ ${PRJ_ROOT}/output/kind-config/local-dev/
 SUBMARINER_BROKER_NS=submariner-k8s-broker
 # FIXME: This can change and break re-running deployments
 SUBMARINER_PSK=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
-export KUBECONFIG=$(echo ${PRJ_ROOT}/output/kind-config/dapper/kind-config-cluster{1..3} | sed 's/ /:/g')
+declare_kubeconfig
 
-kind_clusters "$@"
-setup_custom_cni
+kubectl config view --flatten > ${PRJ_ROOT}/output/kubeconfigs/kind-config-merged
 
 kind_import_images
 setup_broker
@@ -293,12 +221,12 @@ kubectl config use-context $context
 
 # Import functions for testing with Operator
 # NB: These are also used to verify non-Operator deployments, thereby asserting the two are mostly equivalent
-. kind-e2e/lib_operator_verify_subm.sh
+. ${DAPPER_SOURCE}/scripts/kind-e2e/lib_operator_verify_subm.sh
 
 create_subm_vars
 verify_subm_broker_secrets
 
-. kind-e2e/lib_operator_deploy_subm.sh
+. ${DAPPER_SOURCE}/scripts/kind-e2e/lib_operator_deploy_subm.sh
 
 for i in 2 3; do
     context=cluster$i
@@ -311,9 +239,9 @@ for i in 2 3; do
 
     # Deploy SubM Operator
     if [ "${context}" = "cluster2" ] || [ "${context}" = "cluster3" ]; then
-       set -o pipefail
-        ../bin/subctl join --operator-image submariner-operator:local \
-                        --kubeconfig ${PRJ_ROOT}/output/kind-config/dapper/kind-config-merged \
+        set -o pipefail
+        ${DAPPER_SOURCE}/bin/subctl join --operator-image submariner-operator:local \
+                        --kubeconfig ${PRJ_ROOT}/output/kubeconfigs/kind-config-merged \
                         --kubecontext ${context} \
                         --clusterid ${context} \
                         --repository ${subm_engine_image_repo} \
@@ -374,8 +302,8 @@ done
 echo "Running subctl a second time to verify if running subctl a second time works fine"
 
 set -o pipefail
-../bin/subctl join --operator-image submariner-operator:local \
-                --kubeconfig ${PRJ_ROOT}/output/kind-config/dapper/kind-config-merged \
+${DAPPER_SOURCE}/bin/subctl join --operator-image submariner-operator:local \
+                --kubeconfig ${PRJ_ROOT}/output/kubeconfigs/kind-config-merged \
                 --kubecontext ${context} \
                 --clusterid ${context} \
                 --repository ${subm_engine_image_repo} \
@@ -395,16 +323,16 @@ test_connection
 # dataplane E2E need to be modified for globalnet
 if [[ $globalnet = false ]]; then
     # run dataplane E2e tests between the two clusters
-    ../bin/subctl verify-connectivity ${PRJ_ROOT}/output/kind-config/dapper/kind-config-cluster2 \
-                                      ${PRJ_ROOT}/output/kind-config/dapper/kind-config-cluster3 \
+    ${DAPPER_SOURCE}/bin/subctl verify-connectivity ${PRJ_ROOT}/output/kubeconfigs/kind-config-cluster2 \
+                                      ${PRJ_ROOT}/output/kubeconfigs/kind-config-cluster3 \
                                       --verbose
 fi
 
-if [[ $1 = keep ]]; then
+if [[ ${status} = keep ]]; then
     echo "your 3 virtual clusters are deployed and working properly with your local"
     echo "submariner source code, and can be accessed with:"
     echo ""
-    echo "export KUBECONFIG=\$(echo \$(git rev-parse --show-toplevel)/output/kind-config/local-dev/kind-config-cluster{1..3} | sed 's/ /:/g')"
+    echo "export KUBECONFIG=\$(echo \$(git rev-parse --show-toplevel)/output/kubeconfigs/kind-config-cluster{1..3} | sed 's/ /:/g')"
     echo ""
     echo "$ kubectl config use-context cluster1 # or cluster2, cluster3.."
     echo ""
