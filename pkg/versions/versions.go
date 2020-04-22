ceabc63c5a0caccefe3b161a781e4704dfe4e9b2
Aswin Surayanarayanan <asuryana@redhat.com>
2020-04-22 19:20:46 +0530

    Changed lighthouse version to 0.3.0
    Signed-off-by: Aswin Surayanarayanan <asuryana@redhat.com>

--- pkg/versions/versions.go
+++ pkg/versions/versions.go
@@ -4,5 +4,5 @@ const (
 	DefaultSubmarinerRepo            = "quay.io/submariner"
 	DefaultSubmarinerOperatorVersion = "0.2.0"
 	DefaultSubmarinerVersion         = "0.2.0"
-	DefaultLighthouseVersion         = "0.2.0"
+	DefaultLighthouseVersion         = "0.3.0"
 )
