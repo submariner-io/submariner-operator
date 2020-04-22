6ce7da28c3b50c52b85927a2047f012aeaf62315
Miguel Angel Ajo <majopela@redhat.com>
2020-04-22 16:47:04 +0200

    Bump all project versions to 0.3.0-rc1

--- pkg/versions/versions.go
+++ pkg/versions/versions.go
@@ -2,7 +2,7 @@ package versions
 
 const (
 	DefaultSubmarinerRepo            = "quay.io/submariner"
-	DefaultSubmarinerOperatorVersion = "0.2.0"
-	DefaultSubmarinerVersion         = "0.2.0"
+	DefaultSubmarinerOperatorVersion = "0.3.0-rc1"
+	DefaultSubmarinerVersion         = "0.3.0-rc1"
 	DefaultLighthouseVersion         = "0.3.0-rc1"
 )
