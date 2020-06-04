e0eec0a269e8205f5db69a6d4dd74e8a8a5b3088
Aswin Suryanarayanan <asuryana@redhat.com>
2020-06-04 22:08:56 +0530

    Refator the openshift scc update (#439)
    The Openshift SCC code is refactored to avoid duplication.
    
    Signed-off-by: Aswin Surayanarayanan <asuryana@redhat.com>
    
    Co-authored-by: Miguel Angel Ajo Pelayo <majopela@redhat.com>

--- pkg/subctl/operator/lighthouse/scc/ensure.go
+++ pkg/subctl/operator/lighthouse/scc/ensure.go
@@ -17,67 +17,11 @@ limitations under the License.
 package scc
 
 import (
-	"fmt"
-
+	"github.com/submariner-io/submariner-operator/pkg/subctl/operator/common/scc"
 	"github.com/submariner-io/submariner-operator/pkg/subctl/operator/lighthouse/serviceaccount"
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/util/retry"
-)
-
-var (
-	openshiftSCCGVR = schema.GroupVersionResource{
-		Group:    "security.openshift.io",
-		Version:  "v1",
-		Resource: "securitycontextconstraints",
-	}
 )
 
 func Ensure(restConfig *rest.Config, namespace string) (bool, error) {
-
-	dynClient, err := dynamic.NewForConfig(restConfig)
-	if err != nil {
-		return false, err
-	}
-
-	sccClient := dynClient.Resource(openshiftSCCGVR)
-
-	created := false
-	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		cr, err := sccClient.Get("privileged", metav1.GetOptions{})
-		if err != nil {
-			if errors.IsNotFound(err) {
-				return nil
-			}
-			return err
-		}
-		users, found, err := unstructured.NestedSlice(cr.Object, "users")
-		if !found || err != nil {
-			return err
-		}
-
-		submarinerUser := fmt.Sprintf("system:serviceaccount:%s:%s", namespace, serviceaccount.LighthouseServiceAccount)
-
-		for _, user := range users {
-			if submarinerUser == user.(string) {
-				// the user is already part of the scc
-				return nil
-			}
-		}
-
-		if err = unstructured.SetNestedSlice(cr.Object, append(users, submarinerUser), "users"); err != nil {
-			return err
-		}
-
-		if _, err = sccClient.Update(cr, metav1.UpdateOptions{}); err != nil {
-			return fmt.Errorf("Error updating OpenShift privileged SCC: %s", err)
-		}
-		created = true
-		return nil
-	})
-	return created, retryErr
+	return scc.UpateSCC(restConfig, namespace, serviceaccount.LighthouseServiceAccount)
 }
