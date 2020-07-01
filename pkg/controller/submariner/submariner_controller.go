e81ea5258e61c94d1a92cb7539b99d4f05468f34
Maayan Friedman <maafried@redhat.com>
2020-07-01 13:41:45 +0300

    Add subctl show versions cmd
    Resolve issue #471
    
    Signed-off-by: Maayan Friedman <maafried@redhat.com>

--- pkg/controller/submariner/submariner_controller.go
+++ pkg/controller/submariner/submariner_controller.go
@@ -565,7 +565,7 @@ func newServiceDiscoveryCR(namespace string) *submopv1a1.ServiceDiscovery {
 	return &submopv1a1.ServiceDiscovery{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: namespace,
-			Name:      serviceDiscoveryCrName,
+			Name:      ServiceDiscoveryCrName,
 		},
 	}
 }
@@ -593,7 +593,7 @@ const (
 	routeAgentImage        = "submariner-route-agent"
 	engineImage            = "submariner"
 	globalnetImage         = "submariner-globalnet"
-	serviceDiscoveryCrName = "service-discovery"
+	ServiceDiscoveryCrName = "service-discovery"
 )
 
 func getImagePath(submariner *submopv1a1.Submariner, componentImage string) string {
