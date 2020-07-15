078a80633104caf59d3bd08b7e04f95c7c83ff80
Tom Pantelis <tompantelis@gmail.com>
2020-07-15 17:02:52 -0400

    Update for 0.5.0-rc0
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/versions/versions.go
+++ pkg/versions/versions.go
@@ -2,7 +2,7 @@ package versions
 
 const (
 	DefaultSubmarinerRepo            = "quay.io/submariner"
-	DefaultSubmarinerOperatorVersion = "v0.5.0-pre1"
-	DefaultSubmarinerVersion         = "0.4.1"
-	DefaultLighthouseVersion         = "0.4.0"
+	DefaultSubmarinerOperatorVersion = "0.5.0-rc0"
+	DefaultSubmarinerVersion         = "0.5.0-rc0"
+	DefaultLighthouseVersion         = "0.5.0-rc0"
 )
