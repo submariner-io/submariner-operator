b3b0a99edec328f1a8a49443b654ecc003816553
Miguel Angel Ajo <majopela@redhat.com>
2020-08-27 12:30:13 +0200

    Bump Submariner & Lighthouse to 0.6.0
    Signed-off-by: Miguel Angel Ajo <majopela@redhat.com>

--- pkg/versions/versions.go
+++ pkg/versions/versions.go
@@ -2,7 +2,7 @@ package versions
 
 const (
 	DefaultSubmarinerRepo            = "quay.io/submariner"
-	DefaultSubmarinerOperatorVersion = "0.6.0-rc0"
-	DefaultSubmarinerVersion         = "0.6.0-rc0"
-	DefaultLighthouseVersion         = "0.6.0-rc0"
+	DefaultSubmarinerOperatorVersion = "0.6.0"
+	DefaultSubmarinerVersion         = "0.6.0"
+	DefaultLighthouseVersion         = "0.6.0"
 )
