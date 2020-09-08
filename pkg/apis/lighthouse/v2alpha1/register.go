99943fb3d55bca736c3919a61dfeb683a1b1ce46
Steve Mattar <smattar@redhat.com>
2020-09-08 08:22:44 +0300

    style(lighthouse-api): remove unused deadcode `addKnownTypes`
    Signed-off-by: Steve Mattar <smattar@redhat.com>

--- pkg/apis/lighthouse/v2alpha1/register.go
+++ pkg/apis/lighthouse/v2alpha1/register.go
@@ -6,8 +6,6 @@
 package v2alpha1
 
 import (
-	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
@@ -23,21 +21,4 @@ var (
 // Resource takes an unqualified resource and returns a Group qualified GroupResource
 func Resource(resource string) schema.GroupResource {
 	return SchemeGroupVersion.WithResource(resource).GroupResource()
-}
-
-// addKnownTypes adds our types to the API scheme by registering
-// MultiClusterService and MultiClusterServiceList
-func addKnownTypes(scheme *runtime.Scheme) error {
-	scheme.AddKnownTypes(
-		SchemeGroupVersion,
-		&ServiceExport{},
-		&ServiceExportList{},
-		&ServiceImport{},
-		&ServiceImportList{},
-	)
-
-	// register the type in the scheme
-	meta_v1.AddToGroupVersion(scheme, SchemeGroupVersion)
-
-	return nil
 }
