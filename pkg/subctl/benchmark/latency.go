fc44aa51dec8fc7ec20815308faae655479db826
Maayan Friedman <maafried@redhat.com>
2020-09-24 17:39:58 +0300

    Add intra-cluster test to subctl latency benchmark
    Fixes issue #706
    
    Signed-off-by: Maayan Friedman <maafried@redhat.com>

--- pkg/subctl/benchmark/latency.go
+++ pkg/subctl/benchmark/latency.go
@@ -8,7 +8,14 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func StartLatencyTests() {
+type benchmarkTestParams struct {
+	ClusterA            framework.ClusterIndex
+	ClusterB            framework.ClusterIndex
+	ServerPodScheduling framework.NetworkPodScheduling
+	ClientPodScheduling framework.NetworkPodScheduling
+}
+
+func StartLatencyTests(intraCluster bool) {
 	var f *framework.Framework
 
 	gomega.RegisterFailHandler(func(message string, callerSkip ...int) {
@@ -22,48 +29,70 @@ func StartLatencyTests() {
 
 	f = initFramework("latency")
 
-	framework.By("Performing latency tests from Gateway pod to Gateway pod")
-	runLatencyTest(f, framework.GatewayNode)
+	if !intraCluster {
+		latencyTestParams := benchmarkTestParams{
+			ClusterA:            framework.ClusterA,
+			ClusterB:            framework.ClusterB,
+			ServerPodScheduling: framework.GatewayNode,
+			ClientPodScheduling: framework.GatewayNode,
+		}
 
-	framework.By("Performing latency tests from Non-Gateway pod to Non-Gateway pod")
-	runLatencyTest(f, framework.NonGatewayNode)
+		framework.By("Performing latency tests from Gateway pod to Gateway pod")
+		runLatencyTest(f, latencyTestParams)
+
+		latencyTestParams.ServerPodScheduling = framework.NonGatewayNode
+		latencyTestParams.ClientPodScheduling = framework.NonGatewayNode
+		framework.By("Performing latency tests from Non-Gateway pod to Non-Gateway pod")
+		runLatencyTest(f, latencyTestParams)
+	} else {
+		latencyTestIntraClusterParams := benchmarkTestParams{
+			ClusterA:            framework.ClusterA,
+			ClusterB:            framework.ClusterA,
+			ServerPodScheduling: framework.GatewayNode,
+			ClientPodScheduling: framework.NonGatewayNode,
+		}
+
+		clusterAName := framework.TestContext.ClusterIDs[framework.ClusterA]
+		framework.By(fmt.Sprintf("Performing latency tests from Non-Gateway pod to Gateway pod on cluster %q", clusterAName))
+		runLatencyTest(f, latencyTestIntraClusterParams)
+	}
 
 	cleanupFramework(f)
 }
 
-func runLatencyTest(f *framework.Framework, scheduling framework.NetworkPodScheduling) {
-	clusterAName := framework.TestContext.ClusterIDs[framework.ClusterA]
-	clusterBName := framework.TestContext.ClusterIDs[framework.ClusterB]
+func runLatencyTest(f *framework.Framework, testParams benchmarkTestParams) {
+	clusterAName := framework.TestContext.ClusterIDs[testParams.ClusterA]
+	clusterBName := framework.TestContext.ClusterIDs[testParams.ClusterB]
 	var connectionTimeout uint = 5
 	var connectionAttempts uint = 1
 
 	framework.By(fmt.Sprintf("Creating a Nettest Server Pod on %q", clusterBName))
-	nettestPodB := f.NewNetworkPod(&framework.NetworkPodConfig{
+	nettestServerPod := f.NewNetworkPod(&framework.NetworkPodConfig{
 		Type:               framework.LatencyServerPod,
-		Cluster:            framework.ClusterB,
-		Scheduling:         scheduling,
+		Cluster:            testParams.ClusterB,
+		Scheduling:         testParams.ServerPodScheduling,
 		ConnectionTimeout:  connectionTimeout,
 		ConnectionAttempts: connectionAttempts,
 	})
 
-	podsClusterB := framework.KubeClients[framework.ClusterB].CoreV1().Pods(f.Namespace)
-	p1, _ := podsClusterB.Get(nettestPodB.Pod.Name, metav1.GetOptions{})
-	framework.By(fmt.Sprintf("Nettest Server Pod %q was created on node %q", nettestPodB.Pod.Name, nettestPodB.Pod.Spec.NodeName))
+	podsClusterB := framework.KubeClients[testParams.ClusterB].CoreV1().Pods(f.Namespace)
+	p1, _ := podsClusterB.Get(nettestServerPod.Pod.Name, metav1.GetOptions{})
+	framework.By(fmt.Sprintf("Nettest Server Pod %q was created on node %q", nettestServerPod.Pod.Name, nettestServerPod.Pod.Spec.NodeName))
 
 	remoteIP := p1.Status.PodIP
 
-	nettestPodA := f.NewNetworkPod(&framework.NetworkPodConfig{
+	nettestClientPod := f.NewNetworkPod(&framework.NetworkPodConfig{
 		Type:               framework.LatencyClientPod,
-		Cluster:            framework.ClusterA,
-		Scheduling:         scheduling,
+		Cluster:            testParams.ClusterA,
+		Scheduling:         testParams.ClientPodScheduling,
 		RemoteIP:           remoteIP,
 		ConnectionTimeout:  connectionTimeout,
 		ConnectionAttempts: connectionAttempts,
 	})
 
 	framework.By(fmt.Sprintf("Nettest Client Pod %q was created on cluster %q, node %q; connect to server pod ip %q",
-		nettestPodA.Pod.Name, clusterAName, nettestPodA.Pod.Spec.NodeName, remoteIP))
+		nettestClientPod.Pod.Name, clusterAName, nettestClientPod.Pod.Spec.NodeName, remoteIP))
 
-	framework.By(fmt.Sprintf("Waiting for the client pod %q to exit, returning what client sent", nettestPodA.Pod.Name))
-	nettestPodA.AwaitFinish()
+	framework.By(fmt.Sprintf("Waiting for the client pod %q to exit, returning what client sent", nettestClientPod.Pod.Name))
+	nettestClientPod.AwaitFinish()
 }
