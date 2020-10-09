117fc73bd23603caae36471caf5ad25ef462a006
Tom Pantelis <tompantelis@gmail.com>
2020-10-09 09:32:55 -0400

    Bump to submariner 0.7.0-rc1
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/versions/versions.go
+++ pkg/versions/versions.go
@@ -3,6 +3,6 @@ package versions
 const (
 	DefaultSubmarinerRepo            = "quay.io/submariner"
 	DefaultSubmarinerOperatorVersion = "0.7.0-rc1"
-	DefaultSubmarinerVersion         = "0.7.0-rc0"
+	DefaultSubmarinerVersion         = "0.7.0-rc1"
 	DefaultLighthouseVersion         = "0.7.0-rc1"
 )
