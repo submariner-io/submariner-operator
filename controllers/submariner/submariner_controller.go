fea5da84bd68994f92b1e2ebb33c8829d8dee9a6
Vishal Thapar <5137689+vthapar@users.noreply.github.com>
2021-02-02 14:18:42 +0530

    Reconcile ServiceDiscovery before status updates
    Currently ServiceDiscovery reconciler runs after Status reconcilers.
    This can delay service discovery bring up and bugs in status reconciler
    can even block service discovery bring up.
    
    Reconcile service discovery alongwith other submariner components,
    before statuses.
    
    Signed-off-by: Vishal Thapar <5137689+vthapar@users.noreply.github.com>

--- controllers/submariner/submariner_controller.go
+++ controllers/submariner/submariner_controller.go
@@ -153,6 +153,10 @@ func (r *SubmarinerReconciler) Reconcile(request reconcile.Request) (reconcile.R
 		return reconcile.Result{}, err
 	}
 
+	if err := r.serviceDiscoveryReconciler(instance, reqLogger, instance.Spec.ServiceDiscoveryEnabled); err != nil {
+		return reconcile.Result{}, err
+	}
+
 	// Retrieve the gateway information
 	gateways, err := r.retrieveGateways(instance, request.Namespace)
 	if err != nil {
@@ -209,9 +213,6 @@ func (r *SubmarinerReconciler) Reconcile(request reconcile.Request) (reconcile.R
 		}
 	}
 
-	if err := r.serviceDiscoveryReconciler(instance, reqLogger, instance.Spec.ServiceDiscoveryEnabled); err != nil {
-		return reconcile.Result{}, err
-	}
 	return reconcile.Result{}, nil
 }
 
