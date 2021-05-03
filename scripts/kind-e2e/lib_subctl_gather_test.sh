596e70874a3192d2fa46e158bc5d632f8bd65854
Janki Chhatbar <jchhatba@redhat.com>
2021-05-03 14:28:32 +0530

    Add e2e test for log.prev
    Check creation of log file for previously
    terminated pods.
    
    Signed-off-by: Janki Chhatbar <jchhatba@redhat.com>

--- scripts/kind-e2e/lib_subctl_gather_test.sh
+++ scripts/kind-e2e/lib_subctl_gather_test.sh
@@ -73,6 +73,13 @@ function validate_pod_log_files() {
   for pod_name in "${pod_names_array[@]}"; do
     file=$out_dir/${cluster}_$pod_name.log
     cat $file
+
+    # Create log.prev for previous pods
+    restart_count=$(kubectl describe pod $pod_name $nsarg | grep "Restart Count" | awk '{print $3}')
+    if [[ restart_count -gt 0 ]]; then
+      prev_file=$out_dir/${cluster}_$pod_name.log.prev
+      cat $prev_file
+    fi
   done
 }
 
