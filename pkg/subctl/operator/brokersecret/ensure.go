0e02a09e568b31d5dfa0cfc260e63c152189cbbb
Stephen Kitt <skitt@redhat.com>
2021-12-03 11:55:09 +0100

    Store the broker token in a secret
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ pkg/subctl/operator/brokersecret/ensure.go
@@ -0,0 +1,54 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+
+Copyright Contributors to the Submariner project.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package brokersecret
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/submariner-io/admiral/pkg/resource"
+	"github.com/submariner-io/admiral/pkg/util"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientset "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+func Ensure(config *rest.Config, namespace string, brokerSecret *v1.Secret) (*v1.Secret, error) {
+	client, err := clientset.NewForConfig(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating client")
+	}
+
+	// nolint:wrapcheck // No need to wrap errors here
+	object, err := util.CreateAnew(context.TODO(), &resource.InterfaceFuncs{
+		GetFunc: func(ctx context.Context, name string, options metav1.GetOptions) (runtime.Object, error) {
+			return client.CoreV1().Secrets(namespace).Get(ctx, name, options)
+		},
+		CreateFunc: func(ctx context.Context, obj runtime.Object, options metav1.CreateOptions) (runtime.Object, error) {
+			return client.CoreV1().Secrets(namespace).Create(ctx, obj.(*v1.Secret), options)
+		},
+		DeleteFunc: func(ctx context.Context, name string, options metav1.DeleteOptions) error {
+			return client.CoreV1().Secrets(namespace).Delete(ctx, name, options)
+		},
+	}, brokerSecret, metav1.CreateOptions{}, metav1.DeleteOptions{})
+
+	return object.(*v1.Secret), errors.Wrap(err, "error creating Broker secret")
+}
