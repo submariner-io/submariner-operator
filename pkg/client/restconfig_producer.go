8d87d26d098b3468ed44867684b124eced06a783
Tom Pantelis <tompantelis@gmail.com>
2021-12-13 15:24:45 -0500

    Add client Producer interface and implementations
    ...that yields various client set interfaces used throughout the
    code base. This provides a layer of abstraction in lieu of passing
    around rest.Config instances to enable easier reuse for external
    apps, eg the submariner-addon, and to facilitate unit testing.
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

---
+++ pkg/client/restconfig_producer.go
@@ -0,0 +1,55 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+
+Copyright Contributors to the Submariner project.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package client
+
+import (
+	"github.com/pkg/errors"
+	submarinerClientset "github.com/submariner-io/submariner-operator/pkg/client/clientset/versioned"
+	apiextClient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+func NewProducerFromRestConfig(config *rest.Config) (Producer, error) {
+	var err error
+	p := &DefaultProducer{}
+
+	p.KubeClient, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating kube client")
+	}
+
+	p.DynamicClient, err = dynamic.NewForConfig(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating dynamic client")
+	}
+
+	p.SubmarinerClient, err = submarinerClientset.NewForConfig(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating submariner client")
+	}
+
+	p.CRDClient, err = apiextClient.NewForConfig(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating api extensions client")
+	}
+
+	return p, nil
+}
