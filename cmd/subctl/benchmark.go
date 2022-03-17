0914718f4311a4d99f34ee0f48f782535a03ba12
Maxim Babushkin <mbabushk@redhat.com>
2022-03-17 14:16:20 +0200

    Add JUnit report generation to "subctl verify"
    The E2E tests should be able to generate report at the end of the
    execution.
    After the following ginkgo patch - [1], creation of the test directory
    (if required) and generation of the report handled by ginkgo after the
    input parameters such as directory and file name.
    
    Replacing existing parameters which are no longer working after the
    following patch - [2].
    
    [1] - https://github.com/onsi/ginkgo/pull/601
    [2] - https://github.com/submariner-io/shipyard/pull/216
    
    Signed-off-by: Maxim Babushkin <mbabushk@redhat.com>

--- cmd/subctl/benchmark.go
+++ cmd/subctl/benchmark.go
@@ -123,8 +123,7 @@ func setUpTestFramework(args []string, restConfigProducer restconfig.Producer) e
 	framework.TestContext.OperationTimeout = operationTimeout
 	framework.TestContext.ConnectionTimeout = connectionTimeout
 	framework.TestContext.ConnectionAttempts = connectionAttempts
-	framework.TestContext.ReportDir = reportDirectory
-	framework.TestContext.ReportPrefix = "subctl"
+	framework.TestContext.JunitReport = junitReport
 	framework.TestContext.SubmarinerNamespace = constants.SubmarinerNamespace
 
 	config.DefaultReporterConfig.Verbose = verboseConnectivityVerification
