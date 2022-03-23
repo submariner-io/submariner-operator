0ce2929030924a1f0788d8377adafe8bf1554cf2
Vishal Thapar <5137689+vthapar@users.noreply.github.com>
2022-03-23 22:08:10 +0530

    Address review comments and add more tests (2)
    * Addresses minor review comments
    * Adds even more tests
    
    Signed-off-by: Vishal Thapar <5137689+vthapar@users.noreply.github.com>

--- internal/cluster/cluster_test.go
+++ internal/cluster/cluster_test.go
@@ -76,7 +76,7 @@ func testIsValidID() {
 		})
 	})
 
-	When("the id starts or end with a dash", func() {
+	When("the id starts or ends	 with a dash", func() {
 		It("should not be valid", func() {
 			Expect(cluster.IsValidID("-abcdef")).To(Not(Succeed()))
 			Expect(cluster.IsValidID("abcdef-")).To(Not(Succeed()))
@@ -101,36 +101,36 @@ func testIsValidID() {
 func testSanitizeID() {
 	When("the id only contains alphabetic characters", func() {
 		It("should return same id", func() {
-			Expect(cluster.SanitizeID("abcdef")).To(Equal("abcdef"))
+			expectSanitizeIDNoChange("abcdef")
 		})
 	})
 
 	When("the id only contains numeric characters", func() {
 		It("should return same id", func() {
 			Expect(cluster.IsValidID("012345")).To(Succeed())
-			Expect(cluster.SanitizeID("012345")).To(Equal("012345"))
+			expectSanitizeIDNoChange("012345")
 		})
 	})
 
 	When("the id only contains alphanumeric characters", func() {
 		It("should return same id", func() {
-			Expect(cluster.SanitizeID("a0b1c2d3ef")).To(Equal("a0b1c2d3ef"))
-			Expect(cluster.SanitizeID("0a1b2c3def")).To(Equal("0a1b2c3def"))
-			Expect(cluster.SanitizeID("a0b1c2d3e4f5")).To(Equal("a0b1c2d3e4f5"))
-			Expect(cluster.SanitizeID("0a1b2c3d4e5f6")).To(Equal("0a1b2c3d4e5f6"))
+			expectSanitizeIDNoChange("a0b1c2d3ef")
+			expectSanitizeIDNoChange("0a1b2c3def")
+			expectSanitizeIDNoChange("a0b1c2d3e4f5")
+			expectSanitizeIDNoChange("0a1b2c3d4e5f6")
 		})
 	})
 
 	When("the id only contains alphanumeric characters and dashes", func() {
 		It("should return same id valid", func() {
-			Expect(cluster.SanitizeID("a0b-1c2d3ef")).To(Equal("a0b-1c2d3ef"))
-			Expect(cluster.SanitizeID("0a1b2c3-def")).To(Equal("0a1b2c3-def"))
-			Expect(cluster.SanitizeID("a0b1c2d3---e4f5")).To(Equal("a0b1c2d3---e4f5"))
+			expectSanitizeIDNoChange("a0b-1c2d3ef")
+			expectSanitizeIDNoChange("0a1b2c3-def")
+			expectSanitizeIDNoChange("a0b1c2d3---e4f5")
 		})
 	})
 
 	When("the id contains uppercase alphabetic characters", func() {
-		It("should convert lowercase characters to uppercase", func() {
+		It("should convert the characters to lowercase", func() {
 			Expect(cluster.SanitizeID("A1b2c3d4e5f6")).To(Equal("a1b2c3d4e5f6"))
 			Expect(cluster.SanitizeID("1A2b3c4d5e6f")).To(Equal("1a2b3c4d5e6f"))
 			Expect(cluster.SanitizeID("a1-B2-c3-D4")).To(Equal("a1-b2-c3-d4"))
@@ -144,35 +144,55 @@ func testSanitizeID() {
 		})
 	})
 
-	When("the id starts or end with a dash", func() {
+	When("the id starts or ends with a dash", func() {
 		It("should replace dash with 0", func() {
 			Expect(cluster.SanitizeID("-abcdef")).To(Equal("0abcdef"))
 			Expect(cluster.SanitizeID("abcdef-")).To(Equal("abcdef0"))
 		})
 	})
 
-	When("the id starts or end with non alphanumeric character", func() {
+	When("the id starts or ends with non alphanumeric character", func() {
 		It("should replace non alphanumeric character with 0", func() {
 			Expect(cluster.SanitizeID("éabcdef")).To(Equal("0abcdef"))
 			Expect(cluster.SanitizeID("abcdefé")).To(Equal("abcdef0"))
 		})
 	})
 
+	When("the id has consecutive non alphanumeric characters", func() {
+		It("should repalce all such characters with a single dash", func() {
+			Expect(cluster.SanitizeID("a@#$b@#$c")).To(Equal("a-b-c"))
+			Expect(cluster.SanitizeID("a@#$%^&*()._1")).To(Equal("a-1"))
+			Expect(cluster.SanitizeID("@abc!@#123@")).To(Equal("0abc-1230"))
+		})
+	})
+
+	When("the id has no alphanumeric characters", func() {
+		It("should return 0", func() {
+			Expect(cluster.SanitizeID(".@#$_")).To(Equal("0"))
+			Expect(cluster.SanitizeID("@")).To(Equal("0"))
+			Expect(cluster.SanitizeID(".@")).To(Equal("0"))
+		})
+	})
+
 	When("the id is longer than 63 characters", func() {
 		It("should return same id", func() {
 			testID := "0123456789012345678901234567890123456789012345678901234567890123"
-			Expect(cluster.SanitizeID(testID)).To(Equal(testID))
+			expectSanitizeIDNoChange(testID)
 
 			testID = "0123456789012345678901234567890123456789012345678901234567890123456789"
-			Expect(cluster.SanitizeID(testID)).To(Equal(testID))
+			expectSanitizeIDNoChange(testID)
 		})
 	})
 
 	When("the id is empty", func() {
 		It("should return empty string", func() {
-			Expect(cluster.SanitizeID("")).To(Equal(""))
+			expectSanitizeIDNoChange("")
 		})
 	})
+}
+
+func expectSanitizeIDNoChange(id string) {
+	Expect(cluster.SanitizeID(id)).To(Equal(id))
 }
 
 func TestCluster(t *testing.T) {
