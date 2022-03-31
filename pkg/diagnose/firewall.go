28fcf6ec531e1d6ac1273d4cd1d534b7c59c95ef
Tom Pantelis <tompantelis@gmail.com>
2022-03-31 17:42:25 -0400

    Copy pkg/subctl/cmd/diagnose/firewall_tunnel.go to pkg/diagnose
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/diagnose/firewall.go
+++ pkg/diagnose/firewall.go
@@ -19,8 +19,13 @@ limitations under the License.
 package diagnose
 
 import (
+	"context"
+
 	"github.com/pkg/errors"
+	"github.com/submariner-io/submariner-operator/internal/cli"
 	"github.com/submariner-io/submariner-operator/internal/pods"
+	"github.com/submariner-io/submariner-operator/pkg/subctl/cmd"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -59,4 +64,57 @@ func spawnPod(client kubernetes.Interface, scheduling pods.Scheduling, podName, 
 	}
 
 	return pod, nil
+}
+
+func spawnSnifferPodOnNode(client kubernetes.Interface, nodeName, namespace, podCommand string) (*pods.Scheduled, error) {
+	scheduling := pods.Scheduling{
+		ScheduleOn: pods.CustomNode, NodeName: nodeName,
+		Networking: pods.HostNetworking,
+	}
+
+	return spawnPod(client, scheduling, "validate-sniffer", namespace, podCommand)
+}
+
+func getActiveGatewayNodeName(cluster *cmd.Cluster, hostname string, status *cli.Status) string {
+	nodes, err := cluster.KubeClient.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{
+		LabelSelector: "submariner.io/gateway=true",
+	})
+	if err != nil {
+		status.EndWithFailure("Error obtaining the Gateway Nodes in cluster %q: %v", cluster.Name, err)
+		return ""
+	}
+
+	for i := range nodes.Items {
+		node := &nodes.Items[i]
+		if node.Name == hostname {
+			return hostname
+		}
+
+		// On some platforms, the nodeName does not match with the hostname.
+		// Submariner Endpoint stores the hostname info in the endpoint and not the nodeName. So, we spawn a
+		// tiny pod to read the hostname and return the corresponding node.
+		sPod, err := spawnSnifferPodOnNode(cluster.KubeClient, node.Name, "default", "hostname")
+		if err != nil {
+			status.EndWithFailure("Error spawning the sniffer pod on the node %q: %v", node.Name, err)
+			return ""
+		}
+
+		err = sPod.AwaitCompletion()
+
+		sPod.Delete()
+
+		if err != nil {
+			status.EndWithFailure("Error waiting for the sniffer pod to finish its execution on node %q: %v", node.Name, err)
+			return ""
+		}
+
+		if sPod.PodOutput[:len(sPod.PodOutput)-1] == hostname {
+			return node.Name
+		}
+	}
+
+	status.EndWithFailure("Could not find the active Gateway node %q in local cluster in cluster %q",
+		hostname, cluster.Name)
+
+	return ""
 }
