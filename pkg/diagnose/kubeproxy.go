db312b84df9092c49ad17668d20a39710bd893d2
Tom Pantelis <tompantelis@gmail.com>
2022-03-31 19:31:20 -0400

    Refactor pkg/diagnose/kubeproxy.go
    ...as a proper lib function using the new reporter.Interface API.
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/diagnose/kubeproxy.go
+++ pkg/diagnose/kubeproxy.go
@@ -21,10 +21,9 @@ package diagnose
 import (
 	"strings"
 
-	"github.com/spf13/cobra"
-	"github.com/submariner-io/submariner-operator/internal/cli"
+	"github.com/submariner-io/admiral/pkg/reporter"
 	"github.com/submariner-io/submariner-operator/internal/pods"
-	"github.com/submariner-io/submariner-operator/pkg/subctl/cmd"
+	"k8s.io/client-go/kubernetes"
 )
 
 const (
@@ -32,46 +31,30 @@ const (
 	missingInterface          = "ip: can't find device"
 )
 
-func init() {
-	command := &cobra.Command{
-		Use:   "kube-proxy-mode",
-		Short: "Check the kube-proxy mode",
-		Long:  "This command checks if the kube-proxy mode is supported by Submariner.",
-		Run: func(command *cobra.Command, args []string) {
-			cmd.ExecuteMultiCluster(restConfigProducer, checkKubeProxyMode)
-		},
-	}
-
-	addNamespaceFlag(command)
-	diagnoseCmd.AddCommand(command)
-}
-
-func checkKubeProxyMode(cluster *cmd.Cluster) bool {
-	status := cli.NewStatus()
+func KubeProxyMode(client kubernetes.Interface, podNamespace string, status reporter.Interface) bool {
 	status.Start("Checking Submariner support for the kube-proxy mode")
+	defer status.End()
 
 	scheduling := pods.Scheduling{ScheduleOn: pods.GatewayNode, Networking: pods.HostNetworking}
 
 	podOutput, err := pods.ScheduleAndAwaitCompletion(&pods.Config{
 		Name:       "query-iface-list",
-		ClientSet:  cluster.KubeClient,
+		ClientSet:  client,
 		Scheduling: scheduling,
 		Namespace:  podNamespace,
 		Command:    kubeProxyIPVSIfaceCommand,
 	})
 	if err != nil {
-		status.EndWithFailure("Error spawning the network pod: %v", err)
+		status.Failure("Error spawning the network pod: %v", err)
 		return false
 	}
 
-	if strings.Contains(podOutput, missingInterface) {
-		status.QueueSuccessMessage("The kube-proxy mode is supported")
-	} else {
-		status.QueueFailureMessage("The cluster is deployed with kube-proxy ipvs mode which Submariner does not support")
+	if !strings.Contains(podOutput, missingInterface) {
+		status.Failure("The cluster is deployed with kube-proxy ipvs mode which Submariner does not support")
+		return false
 	}
 
-	result := status.ResultFromMessages()
-	status.EndWith(result)
+	status.Success("The kube-proxy mode is supported")
 
-	return result != cli.Failure
+	return true
 }
