ff0330b5789b8ddfc239ca5f8ef58a9dd93a4b81
Tom Pantelis <tompantelis@gmail.com>
2022-04-25 12:24:51 -0400

    Use new show functions in pkg/subctl/cmd/show/show.go
    Signed-Off-By: Janki Chhatbar <jchhatba@redhat.com>
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/subctl/cmd/show/show.go
+++ pkg/subctl/cmd/show/show.go
@@ -20,21 +20,84 @@ package show
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/submariner-io/submariner-operator/cmd/subctl/execute"
 	"github.com/submariner-io/submariner-operator/internal/restconfig"
+	"github.com/submariner-io/submariner-operator/internal/show"
 	"github.com/submariner-io/submariner-operator/pkg/subctl/cmd"
 )
 
 var (
+	restConfigProducer = restconfig.NewProducer()
 	// showCmd represents the show command.
 	showCmd = &cobra.Command{
 		Use:   "show",
-		Short: "Show information about submariner",
-		Long:  `This command shows information about some aspect of the submariner deployment in a cluster.`,
+		Short: "Show information about Submariner",
+		Long:  `This command shows information about some aspect of the Submariner deployment in a cluster.`,
 	}
-	restConfigProducer = restconfig.NewProducer()
+	connectionsCmd = &cobra.Command{
+		Use:     "connections",
+		Short:   "Show cluster connectivity information",
+		Long:    `This command shows information about Submariner endpoint connections with other clusters.`,
+		PreRunE: restConfigProducer.CheckVersionMismatch,
+		Run: func(command *cobra.Command, args []string) {
+			execute.OnMultiCluster(restConfigProducer, execute.IfSubmarinerInstalled(show.Connections))
+		},
+	}
+	endpointsCmd = &cobra.Command{
+		Use:     "endpoints",
+		Short:   "Show Submariner endpoint information",
+		Long:    `This command shows information about Submariner endpoints in a cluster.`,
+		PreRunE: restConfigProducer.CheckVersionMismatch,
+		Run: func(command *cobra.Command, args []string) {
+			execute.OnMultiCluster(restConfigProducer, execute.IfSubmarinerInstalled(show.Endpoints))
+		},
+	}
+	gatewaysCmd = &cobra.Command{
+		Use:     "gateways",
+		Short:   "Show Submariner gateway summary information",
+		Long:    `This command shows summary information about the Submariner gateways in a cluster.`,
+		PreRunE: restConfigProducer.CheckVersionMismatch,
+		Run: func(command *cobra.Command, args []string) {
+			execute.OnMultiCluster(restConfigProducer, execute.IfSubmarinerInstalled(show.Gateways))
+		},
+	}
+	networksCmd = &cobra.Command{
+		Use:     "networks",
+		Short:   "Get information on your cluster related to Submariner",
+		Long:    `This command shows the status of Submariner in your cluster, and the relevant network details from your cluster.`,
+		PreRunE: restConfigProducer.CheckVersionMismatch,
+		Run: func(command *cobra.Command, args []string) {
+			execute.OnMultiCluster(restConfigProducer, show.Network)
+		},
+	}
+	versionCmd = &cobra.Command{
+		Use:     "versions",
+		Short:   "Shows Submariner component versions",
+		Long:    `This command shows the versions of the Submariner components in the cluster.`,
+		PreRunE: restConfigProducer.CheckVersionMismatch,
+		Run: func(command *cobra.Command, args []string) {
+			execute.OnMultiCluster(restConfigProducer, show.Versions)
+		},
+	}
+	allCmd = &cobra.Command{
+		Use:   "all",
+		Short: "Show information related to a Submariner cluster",
+		Long: `This command shows information related to a Submariner cluster:
+		      networks, endpoints, gateways, connections, broker and component versions.`,
+		PreRunE: restConfigProducer.CheckVersionMismatch,
+		Run: func(command *cobra.Command, args []string) {
+			execute.OnMultiCluster(restConfigProducer, show.All)
+		},
+	}
 )
 
 func init() {
 	restConfigProducer.AddKubeConfigFlag(showCmd)
 	cmd.AddToRootCommand(showCmd)
+	showCmd.AddCommand(connectionsCmd)
+	showCmd.AddCommand(endpointsCmd)
+	showCmd.AddCommand(gatewaysCmd)
+	showCmd.AddCommand(networksCmd)
+	showCmd.AddCommand(versionCmd)
+	showCmd.AddCommand(allCmd)
 }
