474e5056d34a0e9b6de7a0c68882bc38638cb1da
Automated Release <release@submariner.io>
2022-05-09 12:40:51 +0000

    Update Operator to use version v0.12.1
    Signed-off-by: Automated Release <release@submariner.io>

--- api/submariner/v1alpha1/versions.go
+++ api/submariner/v1alpha1/versions.go
@@ -20,7 +20,7 @@ package v1alpha1
 
 var (
 	DefaultRepo                      = "quay.io/submariner"
-	DefaultSubmarinerOperatorVersion = "0.12.0"
-	DefaultSubmarinerVersion         = "0.12.0"
-	DefaultLighthouseVersion         = "0.12.0"
+	DefaultSubmarinerOperatorVersion = "0.12.1"
+	DefaultSubmarinerVersion         = "0.12.1"
+	DefaultLighthouseVersion         = "0.12.1"
 )
