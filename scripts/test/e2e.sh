68e989b88910e90dfc9fc454d723c7fba91e45dd
Mike Kolesnik <mkolesni@redhat.com>
2022-06-13 13:18:02 +0300

    Remove white box system testing from E2E
    This will be readded cleanly in the following commit(s) but for now
    simplify E2E testing by removing the code.
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/test/e2e.sh
+++ scripts/test/e2e.sh
@@ -1,56 +1,25 @@
 #!/usr/bin/env bash
 
-## Process command line flags ##
-
-source ${SCRIPTS_DIR}/lib/shflags
-DEFINE_string 'settings' '' "Settings YAML file to customize cluster deployments"
-DEFINE_boolean 'globalnet' false "Deploy with operlapping CIDRs (set to 'true' to enable)"
-FLAGS "$@" || exit $?
-eval set -- "${FLAGS_ARGV}"
-
-settings="${FLAGS_settings}"
-[[ "${FLAGS_globalnet}" = "${FLAGS_TRUE}" ]] && globalnet=true || globalnet=false
+. "${SCRIPTS_DIR}/lib/debug_functions"
+. "${SCRIPTS_DIR}/lib/utils"
+. "${SCRIPTS_DIR}/lib/kubecfg"
 
 set -em -o pipefail
 
-source ${SCRIPTS_DIR}/lib/debug_functions
-source ${SCRIPTS_DIR}/lib/utils
-source ${SCRIPTS_DIR}/lib/deploy_funcs
-source ${SCRIPTS_DIR}/lib/deploy_operator
-
-### Variables ###
-
-[[ ! ${DEPLOY_ARGS} =~ "service-discovery" ]] || lighthouse=true
-timeout="2m" # Used by deploy_resource
+subm_ns=submariner-operator
+settings="${SETTINGS}"
 
 ### Main ###
 
 load_settings
-declare_kubeconfig
+verify="connectivity"
+[[ ! ${DEPLOY_ARGS} =~ "service-discovery" ]] || verify="service-discovery"
+contexts="${clusters[@]}"
 
-# Import functions for testing with Operator
-# NB: These are also used to verify non-Operator deployments, thereby asserting the two are mostly equivalent
-. ${DAPPER_SOURCE}/scripts/test/lib_operator_verify_subm.sh
-
-create_subm_vars
-with_context "$broker" broker_vars
-
-with_context "$broker" verify_subm_broker_secrets
-
-run_subm_clusters verify_subm_deployed
-
-if [[ "$lighthouse" == "true" ]]; then
-    verify="--only service-discovery"
-else
-    verify="--only connectivity"
-fi
-
-# run dataplane E2E tests between the two clusters
-subctl verify ${verify} --submariner-namespace=$subm_ns \
+# Run generic E2E tests between the clusters
+subctl verify --only "${verify}" --submariner-namespace="$subm_ns" \
     --verbose --connection-timeout 20 --connection-attempts 4 \
-    --kubecontexts cluster1,cluster2
+    --kubecontexts "${contexts//${IFS:0:1}/,}"
 
+# Run project specific E2E tests (they don't overlap with the generic ones)
 ${SCRIPTS_DIR}/e2e.sh "$@"
-
-print_clusters_message
-
