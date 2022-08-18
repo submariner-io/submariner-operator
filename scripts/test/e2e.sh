7c002b6f14855e58162370e4c8f4d9f1f7eb4c5b
Stephen Kitt <skitt@redhat.com>
2022-08-18 16:47:29 +0200

    Check the running operator version in e2e
    Once the clusters have been deployed, check that we're actually
    running the version of the operator we expect to test.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- scripts/test/e2e.sh
+++ scripts/test/e2e.sh
@@ -17,6 +17,18 @@ ${SCRIPTS_DIR}/e2e.sh "$@"
 # Reload KUBECONFIG in case deploy was triggered
 . "${SCRIPTS_DIR}/lib/kubecfg"
 
+# Check that we're testing the version we expect
+function check_operator_version() {
+    local opv
+    opv=$(kubectl --context="$cluster" logs -n submariner-operator -l name=submariner-operator --tail=-1 | awk '/Submariner operator version/ { print $NF }')
+    printf "Cluster %s is running operator version %s\n" "$cluster" "$opv"
+    if [ "$opv" != "$VERSION" ]; then
+        printf "Operator version mismatch: expected %s, got %s\n" "$VERSION" "$opv"
+        exit 1
+    fi
+}
+run_consecutive "$*" check_operator_version
+
 command -v subctl || curl -Ls https://get.submariner.io | VERSION=devel bash
 
 load_settings
