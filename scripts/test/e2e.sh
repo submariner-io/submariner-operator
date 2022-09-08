86981c8d33b8e6bd51781548332096fbb53c0a47
Mike Kolesnik <mkolesni@redhat.com>
2022-09-08 14:13:50 +0300

    Fix shellcheck errors and add to linting GHA
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- scripts/test/e2e.sh
+++ scripts/test/e2e.sh
@@ -12,7 +12,7 @@ subm_ns=submariner-operator
 
 # Run project specific E2E tests (they don't overlap with the generic ones)
 # This will deploy the environment if necessary
-${SCRIPTS_DIR}/e2e.sh "$@"
+"${SCRIPTS_DIR}"/e2e.sh "$@"
 
 # Reload KUBECONFIG in case deploy was triggered
 . "${SCRIPTS_DIR}/lib/kubecfg"
@@ -34,7 +34,7 @@ command -v subctl || curl -Ls https://get.submariner.io | VERSION=devel bash
 load_settings
 verify="connectivity"
 [[ "${LIGHTHOUSE}" != "true" ]] || verify="service-discovery"
-contexts="${clusters[@]}"
+contexts="${clusters[*]}"
 
 # Run generic E2E tests between the clusters
 subctl verify --only "${verify}" --submariner-namespace="$subm_ns" \
