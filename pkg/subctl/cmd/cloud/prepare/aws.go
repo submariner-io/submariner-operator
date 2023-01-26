submariner-io/subctl

Only call cloud prepare when it's needed
Since the cloud prepare step opens internal ports and nothing else,
we should only call it when there are ports to open.

Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

pkg/cloud/prepare/aws.go
@@ -54,7 +54,11 @@ func AWS(clusterInfo *cluster.Info, ports *cloud.Ports, config *aws.Config, useL
 				}
 			}
 
-			return cloud.PrepareForSubmariner(input, status)
+			if len(input.InternalPorts) > 0 {
+				return cloud.PrepareForSubmariner(input, status)
+			}
+
+			return nil
 		})
 
 	return status.Error(err, "Failed to prepare AWS cloud")

pkg/cloud/prepare/gcp.go
@@ -48,7 +48,11 @@ func GCP(clusterInfo *cluster.Info, ports *cloud.Ports, config *gcp.Config, useL
 				}
 			}
 
-			return cloud.PrepareForSubmariner(input, status)
+			if len(input.InternalPorts) > 0 {
+				return cloud.PrepareForSubmariner(input, status)
+			}
+
+			return nil
 		})
 
 	return status.Error(err, "Failed to prepare GCP cloud")

pkg/cloud/prepare/rhos.go
@@ -49,7 +49,11 @@ func RHOS(clusterInfo *cluster.Info, ports *cloud.Ports, config *rhos.Config, us
 				}
 			}
 
-			return cloud.PrepareForSubmariner(input, status)
+			if len(input.InternalPorts) > 0 {
+				return cloud.PrepareForSubmariner(input, status)
+			}
+
+			return nil
 		})
 
 	return status.Error(err, "Failed to prepare RHOS cloud")

submariner-io/submariner-operator

pkg/subctl/cmd/cloud/prepare/aws.go
@@ -83,7 +83,11 @@ func prepareAws(cmd *cobra.Command, args []string) {
 				}
 			}
 
-			return cloud.PrepareForSubmariner(input, reporter)
+			if len(input.InternalPorts) > 0 {
+				return cloud.PrepareForSubmariner(input, reporter)
+			}
+
+			return nil
 		})
 
 	exit.OnErrorWithMessage(err, "Failed to prepare AWS cloud")

pkg/subctl/cmd/cloud/prepare/gcp.go
@@ -80,7 +80,11 @@ func prepareGCP(cmd *cobra.Command, args []string) {
 				}
 			}
 
-			return cloud.PrepareForSubmariner(input, reporter)
+			if len(input.InternalPorts) > 0 {
+				return cloud.PrepareForSubmariner(input, reporter)
+			}
+
+			return nil
 		})
 
 	exit.OnErrorWithMessage(err, "Failed to prepare GCP cloud")

pkg/subctl/cmd/cloud/prepare/rhos.go
@@ -82,7 +82,11 @@ func prepareRHOS(cmd *cobra.Command, args []string) {
 				}
 			}
 
-			return cloud.PrepareForSubmariner(input, reporter)
+			if len(input.InternalPorts) > 0 {
+				return cloud.PrepareForSubmariner(input, reporter)
+			}
+
+			return nil
 		})
 
 	exit.OnErrorWithMessage(err, "Failed to prepare RHOS  cloud")