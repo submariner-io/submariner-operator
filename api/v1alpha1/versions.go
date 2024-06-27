70252aca55357055300afebda179b28d2e13c340
Automated Release <release@submariner.io>
2024-06-27 16:18:26 +0000

    Update Operator to v0.15.4
    Signed-off-by: Automated Release <release@submariner.io>

--- api/v1alpha1/versions.go
+++ api/v1alpha1/versions.go
@@ -20,7 +20,7 @@ package v1alpha1
 
 var (
 	DefaultRepo                      = "quay.io/submariner"
-	DefaultSubmarinerOperatorVersion = "0.15.3"
-	DefaultSubmarinerVersion         = "0.15.3"
-	DefaultLighthouseVersion         = "0.15.3"
+	DefaultSubmarinerOperatorVersion = "0.15.4"
+	DefaultSubmarinerVersion         = "0.15.4"
+	DefaultLighthouseVersion         = "0.15.4"
 )
