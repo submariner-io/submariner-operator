9f76b017c58cb00402ff30f39d8a5396b29dd5bb
Automated Release <release@submariner.io>
2026-05-27 16:28:04 +0000

    Update Submariner dependencies to v0.21.3
    Signed-off-by: Automated Release <release@submariner.io>

--- api/v1alpha1/versions.go
+++ api/v1alpha1/versions.go
@@ -20,7 +20,7 @@ package v1alpha1
 
 var (
 	DefaultRepo                      = "quay.io/submariner"
-	DefaultSubmarinerOperatorVersion = "0.21.2"
-	DefaultSubmarinerVersion         = "0.21.2"
-	DefaultLighthouseVersion         = "0.21.2"
+	DefaultSubmarinerOperatorVersion = "0.21.3"
+	DefaultSubmarinerVersion         = "0.21.3"
+	DefaultLighthouseVersion         = "0.21.3"
 )
