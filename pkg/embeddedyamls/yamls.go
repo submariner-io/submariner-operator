98a53ba559cb3abe5bad3d8bfd4a7b8189e4c5fb
Tom Pantelis <tompantelis@gmail.com>
2026-08-10 17:49:24 -0400

    Backport #4166: Grant diagnose Role permission to read Secrets- #4167
    Signed-off-by: Thomas Pantelis <tpanteli@tpanteli-thinkpadt14sgen2i.rmtusfl.csb>

--- pkg/embeddedyamls/yamls.go
+++ pkg/embeddedyamls/yamls.go
@@ -3433,6 +3433,12 @@ rules:
       - get
       - list
   - apiGroups:
+      - ""
+    resources:
+      - secrets
+    verbs:
+      - get
+  - apiGroups:
       - apps
     resources:
       - daemonsets
